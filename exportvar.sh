3e2293edb30dd9ad76e6c9eb844ce4276a0a7bed
nach1116 <nadeem.choudhari@netcracker.com>
2017-08-11 06:34:55 -0400

    update -3

--- exportvar.sh
+++ exportvar.sh
@@ -2,6 +2,18 @@
 
 echo "HELLO WORLD "
 mkdir  ./git-repo
+
+cat /etc/ansible/hosts | grep $SERVER_NAME 
+
+es=$?
+echo "es=$es"
+if [ $es -eq 0 ];then
+ 
+echo -e "[$SERVER_NAME]\n" >> /etc/ansible/hosts
+echo -e "\n $SERVER_NAME ansible_ssh_port=2$JB_HTTP_PORT ansible_ssh_host=$JB_HOST ansible_connection=ssh \n " >> /etc/ansible/hosts
+
+fi
+
 echo JB_HTTP_PORT=$JB_HTTP_PORT > ./git-repo/var.sh
 echo JBOSS_HOME=$JBOSS_HOME >> ./git-repo/var.sh
 echo JAVA_HOME=$JAVA_HOME >> ./git-repo/var.sh
