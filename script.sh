644f44e82b1bbfb672a179d18616a6e464939d47
nach1116 <nadeem.choudhari@netcracker.com>
2017-08-11 06:13:49 -0400

    make changes

--- script.sh
+++ script.sh
@@ -10,6 +10,7 @@ echo --- > $varfile
 echo --- > $varfile2
 echo configure_MN_PN : $configure_MN_PN >> $varfile
 echo JB_HTTP_PORT: $JB_HTTP_PORT >>  $varfile
+echo JB_HTTP_PORT: $JB_HTTP_PORT >>  $varfile2
 JB_HTTPS_PORT=`expr $JB_HTTP_PORT + 1`
 JB_TS_RECOVERY_PORT=`expr $JB_HTTPS_PORT + 1 `
 JB_TS_STATUS_PORT=`expr $JB_TS_RECOVERY_PORT + 1 `
@@ -18,21 +19,33 @@ PN_HOME=` echo $SERVER_HOME_pn `
 DB_NAME=`echo $SERVER_NAME |  tr /a-z/ /A-Z/ `
 echo PN_HOME: $PN_HOME >> $varfile
 echo JB_HTTPS_PORT: $JB_HTTPS_PORT >> $varfile
+echo JB_HTTPS_PORT: $JB_HTTPS_PORT >> $varfile2
 echo JB_TS_RECOVERY_PORT: $JB_TS_RECOVERY_PORT >> $varfile
+echo JB_TS_RECOVERY_PORT: $JB_TS_RECOVERY_PORT >> $varfile2
 echo JB_TS_STATUS_PORT: $JB_TS_STATUS_PORT >> $varfile
+echo JB_TS_STATUS_PORT: $JB_TS_STATUS_PORT >> $varfile2
 echo JB_MANAGEMENT_HTTP_PORT: $JB_MANAGEMENT_HTTP_PORT >> $varfile
+echo JB_MANAGEMENT_HTTP_PORT: $JB_MANAGEMENT_HTTP_PORT >> $varfile2
 echo JBOSS_HOME: $JBOSS_HOME >>  $varfile
+echo JBOSS_HOME: $JBOSS_HOME >>  $varfile2
 echo JAVA_HOME: $JAVA_HOME >> $varfile
+echo JAVA_HOME: $JAVA_HOME >> $varfile2
 echo JB_HOST: $JB_HOST >> $varfile
 echo JB_HOST_SHORT_NAME: $JB_HOST_SHORT_NAME >> $varfile
+echo JB_HOST_SHORT_NAME: $JB_HOST_SHORT_NAME >> $varfile2
 echo SERVER_HOME: $SERVER_HOME >> $varfile
+echo SERVER_HOME: $SERVER_HOME >> $varfile2
 echo SERVER_NAME: $SERVER_NAME >> $varfile2
 echo SERVER_NAME: $SERVER_NAME >> $varfile
 echo DB_HOST: $DB_HOST >> $varfile
+echo DB_HOST: $DB_HOST >> $varfile2
 echo DB_NAME: $DB_NAME >> $varfile
+echo DB_NAME: $DB_NAME >> $varfile2
 echo DB_PORT: $DB_PORT >> $varfile
+echo DB_PORT: $DB_PORT >> $varfile2
 echo WFLY_TMPLT: $WFLY_TMPLT >> $varfile
 echo BUILD1: $BUILD1 >> $varfile
+echo BUILD1: $BUILD1 >> $varfile2
 echo BUILD2: $BUILD2 >> $varfile
 echo BUILD3: $BUILD3 >> $varfile
 echo BUILD4: $BUILD4 >> $varfile
